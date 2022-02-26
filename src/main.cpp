4901582b8e437b2cc719b58f28bf53823ebcbadc
John Romkey <58883+romkey@users.noreply.github.com>
2022-02-26 10:25:11 -0800

    added clang-format options
    formatted code with clang-format
    
    added clangtidy code checks for 'pio check'
    
    fixed bitmap char/uint8_t issues

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include "hw.h"
 
 #include <multiball/app.h>
-#include <multiball/wifi.h>
-#include <multiball/ota_updates.h>
 #include <multiball/homebus.h>
+#include <multiball/ota_updates.h>
+#include <multiball/wifi.h>
 
 #ifdef USE_DIAGNOSTICS
 #include <diagnostics.h>
@@ -24,42 +24,39 @@
 MultiballApp App;
 
 void setup() {
-  const wifi_credential_t wifi_credentials[] = {
-    { WIFI_SSID1, WIFI_PASSWORD1 },
-    { WIFI_SSID2, WIFI_PASSWORD2 },
-    { WIFI_SSID3, WIFI_PASSWORD3 }
-  };
+    const wifi_credential_t wifi_credentials[] = {
+        {WIFI_SSID1, WIFI_PASSWORD1}, {WIFI_SSID2, WIFI_PASSWORD2}, {WIFI_SSID3, WIFI_PASSWORD3}};
 
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  Serial.println("wifis:");
-  Serial.printf("  %32s %32s\n", WIFI_SSID1, WIFI_PASSWORD1);
-  Serial.printf("  %32s %32s\n", WIFI_SSID2, WIFI_PASSWORD2);
-  Serial.printf("  %32s %32s\n", WIFI_SSID3, WIFI_PASSWORD3);
+    Serial.println("wifis:");
+    Serial.printf("  %32s %32s\n", WIFI_SSID1, WIFI_PASSWORD1);
+    Serial.printf("  %32s %32s\n", WIFI_SSID2, WIFI_PASSWORD2);
+    Serial.printf("  %32s %32s\n", WIFI_SSID3, WIFI_PASSWORD3);
 
-  delay(500);
+    delay(500);
 
-  App.wifi_credentials(3, wifi_credentials);
-  App.begin(APP_NAME);
-  Serial.println("[app]");
+    App.wifi_credentials(3, wifi_credentials);
+    App.begin(APP_NAME);
+    Serial.println("[app]");
 
-  furball_setup();
-  Serial.println("[furball]");
+    furball_setup();
+    Serial.println("[furball]");
 
 #ifdef USE_DIAGNOSTICS
-  diagnostics_setup(APP_NAME);
-  Serial.println("[diagnostics]");
+    diagnostics_setup(APP_NAME);
+    Serial.println("[diagnostics]");
 #endif
 
-  delay(500);
+    delay(500);
 }
 
 void loop() {
-  App.handle();
+    App.handle();
 
-  furball_loop();
+    furball_loop();
 
 #ifdef USE_DIAGNOSTICS
-  diagnostics_loop(furball_stream);
+    diagnostics_loop(furball_stream);
 #endif
 }
--- src/storage.cpp
+++ src/storage.cpp
@@ -9,34 +9,33 @@ static bool is_present = false;
 #define LOG_FILENAME "data.txt"
 
 bool storage_begin() {
-  if(logger.begin() && logger.getStatus() != 0xff) {
-    Serial.print("[logger ");
-    Serial.print(logger.getVersion());
-    Serial.println("]");
+    if(logger.begin() && logger.getStatus() != 0xff) {
+        Serial.print("[logger ");
+        Serial.print(logger.getVersion());
+        Serial.println("]");
 
-    Serial.printf("SD status %0x\n", logger.getStatus());
+        Serial.printf("SD status %0x\n", logger.getStatus());
 
-    logger.append(String(LOG_FILENAME));
+        logger.append(String(LOG_FILENAME));
 
-    is_present = true;
-  } else {
-    Serial.println("storage fail");
-    is_present = false;
-  }
+        is_present = true;
+    } else {
+        Serial.println("storage fail");
+        is_present = false;
+    }
 
-  return is_present;
+    return is_present;
 }
 
 bool storage_writeln(const char *str) {
-  if(is_present) {
-    logger.println(str);
-    return true;
-  }
-  else
-    return false;
+    if(is_present) {
+        logger.println(str);
+        return true;
+    } else
+        return false;
 }
 
 void storage_flush() {
-  if(is_present)
-    logger.syncFile();
+    if(is_present)
+        logger.syncFile();
 }
